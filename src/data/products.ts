81f313bcc89b8037e190756d379372333ebcb386
Julio Silva <72465674+juliossantos@users.noreply.github.com>
2025-05-27 15:04:07 -0300

    alterando o valor do produto: 20014

--- src/data/products.ts
+++ src/data/products.ts
@@ -1355,7 +1355,7 @@ export const PRODUCTS: Product[] = [
       {
     "id": "10014",
     "name": "BISC PALITO PREMIUM 40G 1KG",
-    "price": 20.90,
+    "price": 22.99,
     "category": "Biscoito de Queijo",
     "images": ["/products/10014.jpg"],
     "description": "Biscoito de queijo premium (40g). Pacote com aproximadamente 25 unidades.",
